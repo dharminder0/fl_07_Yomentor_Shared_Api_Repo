a2ee40a35ad00ce874123c464d41a9130898f409
Sumit <sumit@fleekitsolutions.com>
2024-03-12 17:04:08 +0530

    no message

--- Hub.Business.Entities/DataModels/Users.cs
+++ Hub.Business.Entities/DataModels/Users.cs
@@ -6,14 +6,22 @@ using System.Threading.Tasks;
 
 namespace Core.Business.Entities.DataModels {
     public class Users {
-        public int id { get; set; }
-        public string  firstname { get; set; }
-        public string  phone { get; set;}
-        public string  email { get; set;} 
-        public DateTime createdate { get; set;}
-        public DateTime updatedate { get; set; }
-        public DateTime lastlogindate { get; set; }
-        public bool isdeleted { get; set; }
-        public int parentid { get; set; }
+        public int Id { get; set; }
+        public string  Firstname { get; set; }
+        public string  Lastname  { get; set; }
+        public string  Phone { get; set;}
+        public string  Email { get; set;}
+        public int Type { get; set; }
+        public DateTime CreateDate { get; set;}
+        public DateTime UpdateDate { get; set; }
+        public DateTime LastLoginDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public int Parentid { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Password { get; set; }
+        public string PasswordSalt { get; set; }
+        public string Token { get; set; }
+        public string  Address { get; set; }
     }
 }
