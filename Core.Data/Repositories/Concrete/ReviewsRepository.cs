c129667e6695a1a5a35fc48e86cdc406dfc2eb89
AnusheelSharma1 <anusheel.sharma@Fleekitsolutions.com>
2024-03-18 16:23:38 +0530

    review Upsert

---
+++ Core.Data/Repositories/Concrete/ReviewsRepository.cs
@@ -0,0 +1,82 @@
+﻿using Core.Business.Entities.DataModels;
+using Core.Common.Data;
+using Core.Data.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Repositories.Concrete
+{
+    public class ReviewsRepository :DataRepository<Reviews>,IReviewsRepository
+    {
+        public async Task<int> InsertReviews(Reviews reviews)
+        {
+            var sql = @"
+        IF NOT EXISTS (SELECT 1 FROM reviews WHERE Id = @Id)
+        BEGIN
+            INSERT INTO Reviews
+            (
+                Addedby,
+                Addedfor,
+                batchid,
+                rating,
+                review,
+                createdate,
+                updatedate,
+                isdeleted
+            )
+            VALUES
+            (
+                @Addedby,
+               @Addedfor,
+               @batchid,
+               @rating,
+               @review,
+               @createdate,
+               '',
+               '0'
+            );
+
+            SELECT SCOPE_IDENTITY();
+        END
+        ELSE
+        BEGIN
+            SELECT Id FROM reviews WHERE Id = @Id;
+        END;
+    ";
+
+            return await ExecuteScalarAsync<int>(sql, reviews);
+        }
+
+        public async Task<int> UpdateReviews(Reviews reviews)
+        {
+            var sql = @"
+        IF EXISTS (SELECT 1 FROM reviews WHERE Id = @Id)
+        BEGIN
+            UPDATE reviews
+            SET
+                Addedby=@addedby,
+                Addedfor=@addedfor,
+                batchid=@batchid,
+                rating=@rating,
+                review=@review,
+                updatedate=GetDate(),
+                isdeleted=@isdeleted
+            WHERE
+                Id = @Id;
+
+            SELECT Id FROM reviews WHERE Id = @Id;
+        END
+        ELSE
+        BEGIN
+            SELECT -1;
+        END;
+    ";
+
+            return await ExecuteScalarAsync<int>(sql, reviews);
+        }
+
+    }
+}
