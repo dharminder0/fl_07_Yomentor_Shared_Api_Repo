970d9646f91add9c7a4108f3cfc88d10dde52500
Anusheel Sharma <158999628+AnusheelSharma1@users.noreply.github.com>
2024-03-26 16:33:34 +0530

    swagger and annoucements

---
+++ Core.Data/Repositories/Abstract/IAnnoucementsRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Business.Entities.DataModels;
+using Core.Common.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Repositories.Abstract
+{
+    public interface IAnnoucementsRepository :IDataRepository<Announcements>
+    {
+    }
+}
---
+++ Core.Data/Repositories/Concrete/AnnoucementsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Business.Entities.DataModels;
+using Core.Common.Data;
+using Core.Data.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Repositories.Concrete
+{
+    public class AnnoucementsRepository : DataRepository<Announcements>, IAnnoucementsRepository
+    {
+    }
+}
